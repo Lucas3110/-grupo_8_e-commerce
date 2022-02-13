3800646cbdfe0fb3bcb138d4fd899717a88e49f6
Lucas3110 <lucas.arg.rodriguez@gmail.com>
2022-02-13 10:25:32 -0300

    arrancando demo edit page

--- routes/mainRoutes.js
+++ routes/mainRoutes.js
@@ -8,6 +8,7 @@ router.get('/login', mainController.login);
 router.get('/productCart', mainController.productCart);
 router.get('/productDetail', mainController.productDetail);
 router.get('/register', mainController.register);
+router.get('/edit', mainController.edit);
 
 
 
