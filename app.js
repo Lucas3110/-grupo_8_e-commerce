aa50f7d214693b53911bbd741f44b14da8a5cf96
Lucas3110 <lucas.arg.rodriguez@gmail.com>
2022-02-26 15:39:49 -0300

    indexroutes borrado

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/mainRoutes');
+var mainRouter = require('./routes/mainRoutes');
 var usersRouter = require('./routes/users');
 
 var app = express();
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.use('/', mainRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
