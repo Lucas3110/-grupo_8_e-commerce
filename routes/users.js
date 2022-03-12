8d9458fedef86ce0bcd6497a95e5fe655d652ab6
Lucas3110 <lucas.arg.rodriguez@gmail.com>
2022-03-12 16:24:55 -0300

    multer primeros pasos funcionado ahora vamos por la validacion

--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,30 @@
 let express = require('express');
-
 let router = express.Router();
+const path = require('path');
+const multer = require('multer');
 const usersController = require('../controllers/usersController');
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, path.join(__dirname, '../public/images/profiles'))
+    },
+    filename: (req, file, cb) => {
+        const newFilename = 'imagen-' + Date.now() + path.extname(file.originalname);
+        cb(null, newFilename);
+    }
+});
+
+const upload = multer({ storage: storage });
+
+
+
 
 
 router.get('/login', usersController.login);
 
 /*** CREATE A USER ***/ 
 router.get('/register', usersController.register);
-router.post('/', usersController.store);
+router.post('/', upload.single('imagen') , usersController.store);
 
 
 
