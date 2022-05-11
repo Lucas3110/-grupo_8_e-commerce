7bdc09f2b1761c362b0d47ec2f2b31c79dae3c51
Jessialbero <jessialbero@gmail.com>
2022-05-11 18:00:13 -0300

    pasando errores al front

--- public/js/userEdit.js
+++ public/js/userEdit.js
@@ -1,15 +1,24 @@
 window.addEventListener('load', function() {
 
     let formulario = document.querySelector(".register-form");
+    let errores = [];
 
 formulario.addEventListener("submit", function(e){
 
     let nombre = document.querySelector('#name');
     if(nombre.value == ""){
-        alert("El nombre debe tener al menos 2 caracteres");
+        errores.push("El nombre debe tener al menos 2 caracteres");
     } else if(nombre.value.length < 2) {
-        alert("El nombre debe tener al menos 2 caracteres");
+        errores.push("El nombre debe tener al menos 2 caracteres");
     }
+
+    let apellido = document.querySelector('#apellido');
+    if(apellido.value == ""){
+        errores.push("El nombre debe tener al menos 2 caracteres");
+    } else if(nombre.value.length < 2) {
+        errores.push("El nombre debe tener al menos 2 caracteres");
+    }     
+
     e.preventDefault();
 
 })    
