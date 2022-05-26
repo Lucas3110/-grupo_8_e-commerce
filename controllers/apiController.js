4bed5734c96ca5a8c1005c6dd02320c189eefbfc
Lucas3110 <lucas.arg.rodriguez@gmail.com>
2022-05-26 20:32:49 -0300

    APIS creadas faltaria hacer bien las carpetas + fixear algunos campos API

---
+++ controllers/apiController.js
@@ -0,0 +1,94 @@
+const db = require('../database/models'); 
+
+
+
+const apis = {
+    productList: function (req, res) {
+        db.Product.findAll({
+            include: [{ association: "collection" }]
+        }).then(products => {
+            let newProduct = products.map(product => {
+                return {
+                    id: product.id,
+                    name: product.name,
+                    description: product.description,
+                    collection: product.collection_id,
+                    detail: "/api/products/" + product.id
+                }
+            })
+            let respuesta = {
+                meta: {
+                    status: 200,
+                    count: products.length, //faltaria el countByCategory
+                    url: "/api/products"
+                },
+                products: newProduct
+            }
+            res.json(respuesta)
+        })
+    },
+    productDetail: function (req, res) {
+        db.Product.findByPk(req.params.id).then(product => {
+            let jsonProducto = {
+                meta: {
+                    status: 200,
+                    url: "/api/products/" + req.params.id
+                },
+                data: {
+                    id: product.id,
+                    name: product.name,
+                    price: product.price,
+                    description: product.description,
+                    image: product.image, //aca habria q poner URL img
+                    collection: product.collection_id
+                }
+            }
+            res.json(jsonProducto);
+        })
+    },
+
+    //APIs USER
+
+    userList: function (req, res) {
+        db.User.findAll({
+            include: [{ association: "category" }]
+        }).then(users => {
+            let newUser = users.map(user => {
+                return {
+                    id: user.id,
+                    name: user.name,
+                    email: user.email,
+                    detail: "/api/users/" + user.id,                    
+                }
+            })
+            let respuesta = {
+                meta: {
+                    status: 200,
+                    count: users.length, //faltaria el countByCategory
+                    url: "/api/users"
+                },
+                users: newUser //chequear q dsp no haya conflicto por cambiarle el name "data"
+            }
+            res.json(respuesta)
+        })
+    },
+    userDetail: function (req, res) {
+        db.User.findByPk(req.params.id).then(user => {
+            let jsonProducto = {
+                meta: {
+                    status: 200,
+                    url: "/api/users/" + req.params.id
+                },
+                data: {
+                    id: user.id,
+                    name: user.name,
+                    last_name: user.last_name,
+                    email: user.email,
+                    image: "/images/users/"+ user.image                   
+                } 
+            }
+            res.json(jsonProducto);
+        })
+    },
+}
+module.exports = apis
---
+++ routes/apiRoutes.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+const apiController= require("../controllers/apiController");
+
+router.get('/users', apiController.userList);
+router.get("/users/:id" , apiController.userDetail);
+router.get('/products', apiController.productList);
+router.get("/products/:id", apiController.productDetail);
+
+module.exports = router;
